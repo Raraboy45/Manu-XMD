b3f93ae61ee0f56f945c8b0324dee913d1ca1fba
Raraboy45 <149242625+Raraboy45@users.noreply.github.com>
2025-02-27 16:16:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&8e9gGZ4a#DjHYhW-vfFkoaNbXgD7iXNhCnHlPFbnrf-SbjtaHfDo' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&FTM2VLgK#bnVGJ4NpNdnHTOaBbrI6HRXxSr4HS44S7g_bak2MXLw' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
